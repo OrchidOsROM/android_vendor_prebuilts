b9ff6a88faefcbc33e5fcab28374a9d25f6188e0
teamorchidos <71541333+teamorchidos@users.noreply.github.com>
2020-11-22 19:20:58 +0000

    new
    new apps

---
+++ prebuilts.mk
@@ -0,0 +1,17 @@
+include $(call first-makefiles-under,$(LOCAL_PATH))
+
+PRODUCT_PACKAGES += \
+    openLauncher \
+    PrivacyBrowser \
+    pep \
+    AccountManager \
+    BlokadaV4 \
+    Metro \
+    OpenCamera \
+    OpenTasks \
+    QKSMS \
+    appwarden \
+    gappbrowser \
+    openLauncher \
+    smart \
+    taptap
